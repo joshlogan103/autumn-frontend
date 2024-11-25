74e5c0ab838d1f55164a8f1f9ab726263e3662c0
Josh Logan <joshlogan103@gmail.com>
2024-11-25 10:01:30 -0500

    create foldering for components, pages, contexts, pages, services and utils

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,15 @@
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  
+  return (
+    <>
+      <div className="page">
+        <div className="welcome">
+          Autumn</div>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage;
