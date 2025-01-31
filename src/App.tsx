c8b33d4c72c3d46885b9bb2c3ea7178a676fad4a
Josh Logan <joshlogan103@gmail.com>
2025-01-30 19:04:07 -0500

    add poc3

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
+// import AboutPage from "./pages/AboutPage/AboutPage";
 import PoC from "./pages/PoC/PoC";
-import PoC2 from "./pages/PoC2/PoC2"
+import PoC2 from "./pages/PoC2/PoC2";
+import PoC3 from "./pages/PoC3/PoC3";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <div className="body-container">
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/poc" element={<PoC />} />
+            {/* <Route path="/about" element={<AboutPage />} /> */}
+            <Route path="/champify" element={<PoC />} />
             <Route path="/scalero" element={<PoC2 />} />
+            <Route path="/simondata" element={<PoC3 />} />
           </Routes>
         </div>
       </div>
